9424d443f7a2d6c39f3ea9b769a7f303c8a55e4f
Jeremy Drager <jdrager2@gmail.com>
2020-03-05 22:11:10 -0500

    Adding Get and Update (#3)
    * Test change
    
    * Comment out apiKeys
    
    * Add Filter to the Hold CI step
    
    * Fix yaml
    
    * Added git and update

--- broadcast/update.js
+++ broadcast/update.js
@@ -1,11 +1,11 @@
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.update = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Go Serverless v1.0! Your function executed successfully!  Update',
         input: event,
       },
       null,
