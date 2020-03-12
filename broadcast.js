f068c2b9e41a7e964be35b782d96e2161270a898
Jeremy Drager <jdrager2@gmail.com>
2020-03-12 15:37:29 -0400

    Updated Broadcast to better post

--- broadcast.js
+++ broadcast.js
@@ -41,51 +41,59 @@ const get = async event => {
 
 const update = async event => {
 
-const params = {
-  TableName: process.env.DYNAMODB_TABLE,
-  Key: {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
       id: event.pathParameters.id,
-  },
-  // ExpressionAttributeNames: {
-  //     '#todo_text': 'text',
-  // },
-  ExpressionAttributeValues: {
-      ':name': data.gameStartTime,
-      ':longName': data.longName,
-      ':primaryColor': primaryColor,
-      ':secondaryColor': secondaryColor
-  },
-  UpdateExpression: 'SET name = :name, longName = :longName, primaryColor = :primaryColor, secondaryColor = :secondaryColor',
-  ReturnValues: 'ALL_NEW',
-};
+    },
+    // ExpressionAttributeNames: {
+    //     '#todo_text': 'text',
+    // },
+    ExpressionAttributeValues: {
+      ':period':          data.period,
+      ':time':            data.time,
+      ':home.name':       data.homeTeam.name,
+      ':home.logo':       data.homeTeam.logo,
+      ':home.color':      data.homeTeam.color,
+      ':home.gamesWon':   data.homeTeam.gamesWon,
+      ':home.score':      data.homeTeam.score,
+      ':away.name':       data.awayTeam.name,
+      ':away.logo':       data.awayTeam.logo,
+      ':away.color':      data.awayTeam.color,
+      ':away.gamesWon':   data.awayTeam.gamesWon,
+      ':away.score':      data.awayTeam.score,
+      ':play.name':       data.playByPlay.name,
+      ':play.title':      data.playByPlay.title,
+      ':play.subtitle':   data.playByPlay.subtitle,
+      ':color.name':      data.colorCommentary.name,
+      ':color.title':     data.colorCommentary.title,
+      ':color.subtitle':  data.colorCommentary.subtitle,
+      ':field.name':      data.fieldReporter.name,
+      ':field.title':     data.fieldReporter.title,
+      ':field.subtitle':  data.fieldReporter.subtitle
 
-// update the todo in the database
-dynamoDb.update(params, (error, result) => {
-  // handle potential errors
-  if (error) {
-      console.error(error);
-      callback(null, {
-          statusCode: error.statusCode || 501,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Couldn\'t fetch the team.',
-      });
-      return;
-  }
+    },
+    UpdateExpression: "set period=:period, time=:time, " +
+                            "homeTeam.name=:home.name, homeTeam.logo=:home.logo, homeTeam.color=:home.color, homeTeam.gamesWon=:home.gamesWon, homeTeam.score=:home.score, " +
+                            "awayTeam.name=:away.name, awayTeam.logo=:away.logo, awayTeam.color=:away.color, awayTeam.gamesWon=:away.gamesWon, awayTeam.score=:away.score, " +
+                            "playByPlay.name=:play.name, playByPlay.title=:play.title, playByPlay.subtitle=:play.subtitle, " +
+                            "colorCommentary.name=:color.name, colorCommentary.title=:color.title, colorCommentary.subtitle=:color.subtitle, " +
+                            "fieldReporter.name=:field.name, fieldReporter.title=:field.title, fieldReporter.subtitle=:field.subtitle, "
+    ,ReturnValues: 'UPDATED_NEW',
+  };
 
-  
-  // write the team to the database
-  dynamoDb.put(params, (error) => {
+  // update the todo in the database
+  dynamoDb.update(params, (error, result) => {
     // handle potential errors
     if (error) {
       console.error(error);
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t save broadcast data.  Message: ' + JSON.stringify(error)
+        body: 'Couldn\'t save broadcast data.  Message: ' + JSON.stringify(error),
       });
-      result = data;
+      return;
     }
-
 
     return {
       statusCode: 200,
