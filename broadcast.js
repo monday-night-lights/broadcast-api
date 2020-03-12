1804b945a0f1fb3e47098a8bc2bea30444deb22e
Jeremy Drager <jdrager2@gmail.com>
2020-03-12 16:35:38 -0400

    Remove UUID

--- broadcast.js
+++ broadcast.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const uuid = require('uuid');
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
