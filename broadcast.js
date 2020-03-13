8627d65a5c7873fceab56ef8b699c70a74100275
Jeremy Drager <jdrager2@gmail.com>
2020-03-12 21:59:38 -0400

    Removed callback, retrieved data from event

--- broadcast.js
+++ broadcast.js
@@ -17,12 +17,11 @@ const get = async event => {
     // handle potential errors
     if (error) {
       console.error(error);
-      callback(null, {
+      return {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
         body: 'Couldn\'t fetch the broadcast data.  Message: ' + error.message
-      });
-      return;
+      };
     }
 
     return {
@@ -40,39 +39,41 @@ const get = async event => {
 
 const create = async event => {
 
+  const data = event.body;
+
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
     ExpressionAttributeValues: {
-      ':period':          data.period,
-      ':time':            data.time,
-      ':home.name':       data.homeTeam.name,
-      ':home.logo':       data.homeTeam.logo,
-      ':home.color':      data.homeTeam.color,
-      ':home.gamesWon':   data.homeTeam.gamesWon,
-      ':home.score':      data.homeTeam.score,
-      ':away.name':       data.awayTeam.name,
-      ':away.logo':       data.awayTeam.logo,
-      ':away.color':      data.awayTeam.color,
-      ':away.gamesWon':   data.awayTeam.gamesWon,
-      ':away.score':      data.awayTeam.score,
-      ':play.name':       data.playByPlay.name,
-      ':play.title':      data.playByPlay.title,
-      ':play.subtitle':   data.playByPlay.subtitle,
-      ':color.name':      data.colorCommentary.name,
-      ':color.title':     data.colorCommentary.title,
-      ':color.subtitle':  data.colorCommentary.subtitle,
-      ':field.name':      data.fieldReporter.name,
-      ':field.title':     data.fieldReporter.title,
-      ':field.subtitle':  data.fieldReporter.subtitle
+      ':period': data.period,
+      ':time': data.time,
+      ':home.name': data.homeTeam.name,
+      ':home.logo': data.homeTeam.logo,
+      ':home.color': data.homeTeam.color,
+      ':home.gamesWon': data.homeTeam.gamesWon,
+      ':home.score': data.homeTeam.score,
+      ':away.name': data.awayTeam.name,
+      ':away.logo': data.awayTeam.logo,
+      ':away.color': data.awayTeam.color,
+      ':away.gamesWon': data.awayTeam.gamesWon,
+      ':away.score': data.awayTeam.score,
+      ':play.name': data.playByPlay.name,
+      ':play.title': data.playByPlay.title,
+      ':play.subtitle': data.playByPlay.subtitle,
+      ':color.name': data.colorCommentary.name,
+      ':color.title': data.colorCommentary.title,
+      ':color.subtitle': data.colorCommentary.subtitle,
+      ':field.name': data.fieldReporter.name,
+      ':field.title': data.fieldReporter.title,
+      ':field.subtitle': data.fieldReporter.subtitle
 
     },
     UpdateExpression: "set period=:period, time=:time, " +
-                            "homeTeam.name=:home.name, homeTeam.logo=:home.logo, homeTeam.color=:home.color, homeTeam.gamesWon=:home.gamesWon, homeTeam.score=:home.score, " +
-                            "awayTeam.name=:away.name, awayTeam.logo=:away.logo, awayTeam.color=:away.color, awayTeam.gamesWon=:away.gamesWon, awayTeam.score=:away.score, " +
-                            "playByPlay.name=:play.name, playByPlay.title=:play.title, playByPlay.subtitle=:play.subtitle, " +
-                            "colorCommentary.name=:color.name, colorCommentary.title=:color.title, colorCommentary.subtitle=:color.subtitle, " +
-                            "fieldReporter.name=:field.name, fieldReporter.title=:field.title, fieldReporter.subtitle=:field.subtitle, "
-    ,ReturnValues: 'ALL_NEW',
+      "homeTeam.name=:home.name, homeTeam.logo=:home.logo, homeTeam.color=:home.color, homeTeam.gamesWon=:home.gamesWon, homeTeam.score=:home.score, " +
+      "awayTeam.name=:away.name, awayTeam.logo=:away.logo, awayTeam.color=:away.color, awayTeam.gamesWon=:away.gamesWon, awayTeam.score=:away.score, " +
+      "playByPlay.name=:play.name, playByPlay.title=:play.title, playByPlay.subtitle=:play.subtitle, " +
+      "colorCommentary.name=:color.name, colorCommentary.title=:color.title, colorCommentary.subtitle=:color.subtitle, " +
+      "fieldReporter.name=:field.name, fieldReporter.title=:field.title, fieldReporter.subtitle=:field.subtitle, "
+    , ReturnValues: 'ALL_NEW',
   };
 
   // update the todo in the database
@@ -80,12 +81,11 @@ const create = async event => {
     // handle potential errors
     if (error) {
       console.error(error);
-      callback(null, {
+      return {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
         body: 'Couldn\'t save broadcast data.  Message: ' + JSON.stringify(error),
-      });
-      return;
+      };
     }
 
     return {
@@ -107,6 +107,8 @@ const create = async event => {
 
 const update = async event => {
 
+  const data = event.body;
+
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
     Key: {
@@ -116,36 +118,36 @@ const update = async event => {
     //     '#todo_text': 'text',
     // },
     ExpressionAttributeValues: {
-      ':period':          data.period,
-      ':time':            data.time,
-      ':home.name':       data.homeTeam.name,
-      ':home.logo':       data.homeTeam.logo,
-      ':home.color':      data.homeTeam.color,
-      ':home.gamesWon':   data.homeTeam.gamesWon,
-      ':home.score':      data.homeTeam.score,
-      ':away.name':       data.awayTeam.name,
-      ':away.logo':       data.awayTeam.logo,
-      ':away.color':      data.awayTeam.color,
-      ':away.gamesWon':   data.awayTeam.gamesWon,
-      ':away.score':      data.awayTeam.score,
-      ':play.name':       data.playByPlay.name,
-      ':play.title':      data.playByPlay.title,
-      ':play.subtitle':   data.playByPlay.subtitle,
-      ':color.name':      data.colorCommentary.name,
-      ':color.title':     data.colorCommentary.title,
-      ':color.subtitle':  data.colorCommentary.subtitle,
-      ':field.name':      data.fieldReporter.name,
-      ':field.title':     data.fieldReporter.title,
-      ':field.subtitle':  data.fieldReporter.subtitle
+      ':period': data.period,
+      ':time': data.time,
+      ':home.name': data.homeTeam.name,
+      ':home.logo': data.homeTeam.logo,
+      ':home.color': data.homeTeam.color,
+      ':home.gamesWon': data.homeTeam.gamesWon,
+      ':home.score': data.homeTeam.score,
+      ':away.name': data.awayTeam.name,
+      ':away.logo': data.awayTeam.logo,
+      ':away.color': data.awayTeam.color,
+      ':away.gamesWon': data.awayTeam.gamesWon,
+      ':away.score': data.awayTeam.score,
+      ':play.name': data.playByPlay.name,
+      ':play.title': data.playByPlay.title,
+      ':play.subtitle': data.playByPlay.subtitle,
+      ':color.name': data.colorCommentary.name,
+      ':color.title': data.colorCommentary.title,
+      ':color.subtitle': data.colorCommentary.subtitle,
+      ':field.name': data.fieldReporter.name,
+      ':field.title': data.fieldReporter.title,
+      ':field.subtitle': data.fieldReporter.subtitle
 
     },
     UpdateExpression: "set period=:period, time=:time, " +
-                            "homeTeam.name=:home.name, homeTeam.logo=:home.logo, homeTeam.color=:home.color, homeTeam.gamesWon=:home.gamesWon, homeTeam.score=:home.score, " +
-                            "awayTeam.name=:away.name, awayTeam.logo=:away.logo, awayTeam.color=:away.color, awayTeam.gamesWon=:away.gamesWon, awayTeam.score=:away.score, " +
-                            "playByPlay.name=:play.name, playByPlay.title=:play.title, playByPlay.subtitle=:play.subtitle, " +
-                            "colorCommentary.name=:color.name, colorCommentary.title=:color.title, colorCommentary.subtitle=:color.subtitle, " +
-                            "fieldReporter.name=:field.name, fieldReporter.title=:field.title, fieldReporter.subtitle=:field.subtitle, "
-    ,ReturnValues: 'UPDATED_NEW',
+      "homeTeam.name=:home.name, homeTeam.logo=:home.logo, homeTeam.color=:home.color, homeTeam.gamesWon=:home.gamesWon, homeTeam.score=:home.score, " +
+      "awayTeam.name=:away.name, awayTeam.logo=:away.logo, awayTeam.color=:away.color, awayTeam.gamesWon=:away.gamesWon, awayTeam.score=:away.score, " +
+      "playByPlay.name=:play.name, playByPlay.title=:play.title, playByPlay.subtitle=:play.subtitle, " +
+      "colorCommentary.name=:color.name, colorCommentary.title=:color.title, colorCommentary.subtitle=:color.subtitle, " +
+      "fieldReporter.name=:field.name, fieldReporter.title=:field.title, fieldReporter.subtitle=:field.subtitle, "
+    , ReturnValues: 'UPDATED_NEW',
   };
 
   // update the todo in the database
@@ -153,12 +155,11 @@ const update = async event => {
     // handle potential errors
     if (error) {
       console.error(error);
-      callback(null, {
+      return {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
         body: 'Couldn\'t save broadcast data.  Message: ' + JSON.stringify(error),
-      });
-      return;
+      };
     }
 
     return {
